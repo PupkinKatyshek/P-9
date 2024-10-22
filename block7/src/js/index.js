147c80e07addd464091374b941fc7f982d6f9737
PupkinKatyshek <151539622+PupkinKatyshek@users.noreply.github.com>
2024-10-22 16:02:50 +0300

    f6

--- block7/src/js/index.js
+++ block7/src/js/index.js
@@ -2,7 +2,6 @@ import 'swiper/swiper.scss'
 import 'swiper/modules/pagination.scss'
 import 'swiper/modules/mousewheel.scss'
 import '../scss/style.scss'
-
 import Swiper from 'swiper'
 import { Pagination, Mousewheel } from 'swiper/modules'
 
@@ -13,19 +12,16 @@ let mySwiper
 
 function mobileSlider() {
   if (window.innerWidth <= 768 && slider.dataset.mobile == 'false') {
-    mySwiper = new Swiper(slider, {
+    mySwiper = new Swiper('.slaider1', {
       slidesPerView: 'auto',
       spaceBetween: 17,
       loop: true,
       slideClass: 'card',
       slidesOffsetBefore: 16,
-      // slidesOffsetAfter: 67,
-      // centeredSlides: false,
       modules: [Mousewheel, Pagination],
       mousewheel: {
         enabled: true
       },
-      // modules: [Pagination],
       pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
@@ -50,19 +46,35 @@ mobileSlider()
 window.addEventListener('resize', () => {
   mobileSlider()
 })
+let about = document.querySelector('.about__text')
+let toogle = document.querySelectorAll('.viewmore')
+let but = document.querySelectorAll('.icon-text')
+let strelochka = document.querySelectorAll('.icon-button')
+let toogle0 = toogle[0]
+let toogle1 = toogle[1]
 
-let toogle = document.querySelector('.viewmore')
-let but = document.querySelector('.icon-text')
-let strelochka = document.querySelector('.icon-button')
-
-toogle.onclick = function () {
+toogle1.onclick = function () {
+  console.log('but[0]:', but[0].textContent, 'but[1]:', but[1].textContent)
   swiper.classList.toggle('swiper-wrapper--vesb-blok')
   swiper.classList.toggle('swiper-wrapper--polovinka')
-  if (but.textContent == 'Показать всё') {
-    but.textContent = 'Скрыть'
-    strelochka.style.webkitTransform = 'rotate(180deg)'
-  } else if (but.textContent == 'Скрыть') {
-    but.textContent = 'Показать всё'
-    strelochka.style.webkitTransform = 'rotate(0deg)'
+  if (but[1].textContent == 'Показать всё') {
+    but[1].textContent = 'Скрыть'
+    strelochka[1].style.webkitTransform = 'rotate(180deg)'
+  } else if (but[1].textContent == 'Скрыть') {
+    but[1].textContent = 'Показать всё'
+    strelochka[1].style.webkitTransform = 'rotate(0deg)'
+  }
+}
+
+toogle0.onclick = function () {
+  console.log('but[0]:', but[0].textContent, 'but[1]:', but[1].textContent)
+  about.classList.toggle('about__text--vesb-blok')
+  about.classList.toggle('about__text--polovinka')
+  if (but[0].textContent == 'Показать всё') {
+    but[0].textContent = 'Скрыть'
+    strelochka[0].style.webkitTransform = 'rotate(180deg)'
+  } else if (but[0].textContent == 'Скрыть') {
+    but[0].textContent = 'Показать всё'
+    strelochka[0].style.webkitTransform = 'rotate(0deg)'
   }
 }
